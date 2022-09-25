3f472ee17e07ee5ff1e47c26a65af1cc1508cf7b
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2022-09-25 00:30:04 -0300

    update tests

--- src/Proxy/HttpClient/GoogleHttpClient.php
+++ src/Proxy/HttpClient/GoogleHttpClient.php
@@ -6,12 +6,5 @@ use Psr\Http\Message\ResponseInterface;
 
 interface GoogleHttpClient
 {
-    /**
-     * Gets the ResponseInterface for the informed URL based on all the information that
-     * the proxy service needs
-     *
-     * @param string $url
-     * @return ResponseInterface
-     */
     public function getHttpResponse(string $url): ResponseInterface;
 }
